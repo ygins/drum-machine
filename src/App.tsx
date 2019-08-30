d6288496a345af272753a2fc87a97222335c1302
Yonatan <yginsburg26@gmail.com>
2019-08-29 21:43:52 -0400

    Themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+interface Props {
 
-export default App;
+};
+interface State {
+
+};
+export default class App extends React.Component<Props, State>{
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
---
+++ src/components/DrumMachine/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+
+};
+
+interface State {
+
+};
+
+export default class DrumMachine extends React.Component<Props, State>{
+  constructor(props: Props) {
+    super(props);
+  }
+}
